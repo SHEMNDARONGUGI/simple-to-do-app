1de298f674a599a92fc11b03d1b392ee4f16a670
SHEMNDARONGUGI <ndaroshem@gmal.com>
2025-10-17 23:55:57 +0300

    Initial commit

--- mytodo/mytodo/settings.py
+++ mytodo/mytodo/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wa=6o^rv7!z7#53c-2wvx+w=cjquxk68&!7+ovi$d6z*$b2!!d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mytodoapp.onrender.com']
 
 
 # Application definition
